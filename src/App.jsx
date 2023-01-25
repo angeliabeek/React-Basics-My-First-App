03577f48c34574933bea26fce3a20d50df78bacb
Angelia Beek <angeliabeek@outlook.com>
2023-01-25 17:36:18 +0100

    Exercise Using state to display search results

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import { DrinkButtons } from "./components/DrinkButtons";
 import { DrinkChoice } from "./components/DrinkChoice";
+import { DrinkSearch } from "./components/DrinkSearch";
 import { coffee, tea } from "./utils/data";
 
 export const App = () => {
-  const [userDrink, setUserDrink] = useState(tea);
+  const [userDrink, setUserDrink] = useState();
 
   const greeting = "Welcome in our cafe";
 
@@ -17,6 +18,7 @@ export const App = () => {
         <>
           <h1>{greeting}</h1>
           <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+          <DrinkSearch />
         </>
       )}
     </div>
